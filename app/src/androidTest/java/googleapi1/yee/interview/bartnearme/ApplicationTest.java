fa1f89625946b2df22e3d43ac99340bc08f049a0
tofywing <tofywing@gmail.com>
2016-02-09 11:00:01 -0800

    Commit

---
+++ app/src/androidTest/java/googleapi1/yee/interview/bartnearme/ApplicationTest.java
@@ -0,0 +1,13 @@
+package googleapi1.yee.interview.bartnearme;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
